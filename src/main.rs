78b11890b577d8eac948546f43f1299d8e949bad
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-04-01 19:27:26 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
-
-// src/main.rs
-
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
 
 static HELLO: &[u8] = b"Hello World!";
 
@@ -28,3 +20,8 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
