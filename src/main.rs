6fbbe1af0543f1d8b8405b871f92051d24459969
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-04-06 11:28:48 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,49 @@
 
 mod vga_buffer;
 use core::panic::PanicInfo;
+use std::{print, println};
 
 static HELLO: &[u8] = b"Hello World!";
 
+#![reexport_test_harness_main = "test_main"]
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock() , ", some numbers : {} {} "   ,50, 2.55).unwrap();
-    write!(vga_buffer::WRITER.lock() , ",~\n\n\n some numbers : {} {} "   ,50, 2.55).unwrap();
+    print!("hello world ");
+    for i in 0..5 {
+        println!("Hello World{} {}", "!", i);
+    }
 
-
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!(" panic {} ", info);
     loop {}
+}
+
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+
+
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion ... " );
+    assert_eq!(1, 1);
+    println!("[OK]")
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,8 +1,8 @@
 use core::fmt;
 
+use lazy_static::lazy_static;
 use spin::Mutex;
 use volatile::Volatile;
-use lazy_static::lazy_static;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -59,15 +59,13 @@ pub struct Writer {
     buffer: &'static mut Buffer,
 }
 
-
 lazy_static! {
-    pub static ref WRITER:Mutex<Writer>  =  Mutex::new( Writer {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
 }
-
 
 impl Writer {
     pub fn write_byte(&mut self, byte: u8) {
@@ -138,3 +136,25 @@ pub fn print_something() {
     writer.write_string("world");
     writer.write_string("Wörld!");
 }
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
+
+
+
+
