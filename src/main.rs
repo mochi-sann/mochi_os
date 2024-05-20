9fcde19b727cf10fd87ff5b029e4a5d1fed9444a
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-05-20 19:19:38 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,18 @@ use vga_buffer::print_something;
 mod vga_buffer;
 
 
+#![reexport_test_harness_main = "test_main"]
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
 
     use core::fmt::Write; 
 
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -22,3 +28,16 @@ fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
+
+#[feature(custom_test_frameworks)]
+#[test_runner(crate::test_runner)]
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+
